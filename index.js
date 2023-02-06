9c2e55de8ed516b6e08094b1d81effa9f97d0d7b
Erick <erick.imai.rodrigues@gmail.com>
2023-02-06 10:14:21 -0300

    Fixed bad query request error

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const port = process.env.PORT || 3000;
-const axios = require('axios');
+const express = require("express");
+const port = process.env.PORT || 3030;
+const axios = require("axios");
 const app = express();
 const dungeonIds = [6932, 7672, 13982, 14032, 13954, 8079, 14063, 5965];
 
-async function sortObjectbyValue(obj={},asc=true){ 
+async function sortObjectbyValue(obj = {}, asc = true) {
   const ret = {};
-  Object.keys(obj).sort((a,b) => obj[asc?b:a]-obj[asc?a:b]).forEach(s => ret[s] = obj[s]);
-  return ret
+  Object.keys(obj)
+    .sort((a, b) => obj[asc ? b : a] - obj[asc ? a : b])
+    .forEach((s) => (ret[s] = obj[s]));
+  return ret;
 }
 
 async function fetchRuns(characterId, dungeonId) {
-  const response = await axios.get(`https://raider.io/api/characters/mythic-plus-runs?season=season-df-1&characterId=${characterId}&dungeonId=${dungeonId}`);
+  const response = await axios.get(
+    `https://raider.io/api/characters/mythic-plus-runs?season=season-df-1&characterId=${characterId}&dungeonId=${dungeonId}`
+  );
   return response.data.runs;
 }
 
 async function fetchRunDetails(keystoneRunId) {
-  const response = await axios.get(`https://raider.io/api/mythic-plus/runs/season-df-1/${keystoneRunId}`);
+  const response = await axios.get(
+    `https://raider.io/api/mythic-plus/runs/season-df-1/${keystoneRunId}`
+  );
   return response.data.keystoneRun.roster;
 }
 
@@ -40,20 +46,24 @@ async function main(characterId) {
   return sortedData;
 }
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.static("views"));
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.get('/', async (req, res) => {
-  res.render('index');
+app.get("/", async (req, res) => {
+  res.render("index");
 });
 
-app.get('/:characterId', async (req, res) => {
+app.get("/:characterId", async (req, res) => {
   const characterId = req.query.characterId;
-  const ret = await main(characterId);
-  res.render('data', { data: ret });
+  if (characterId !== undefined) {
+    const ret = await main(characterId);
+    console.log(ret);
+    res.render("data", { data: ret });
+  }
 });
